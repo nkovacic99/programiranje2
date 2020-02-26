8479563695ef1969981867ade3506d69b2069573
nkovacic99 <nace.kovacic007@gmail.com>
2020-02-26 21:03:46 +0100

    vaje

---
+++ Vaje.java
@@ -0,0 +1,16 @@
+public class Vaje {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println(vsota(21, 21));
+        System.out.println(deli(42, 3));
+    }
+
+    public static int vsota(int a, int b) {
+        return a + b;
+    }
+
+    public static int deli(int a, int b) {
+        return a / b;
+    }
+}
