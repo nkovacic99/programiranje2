00695af14b2fdbdce2c2865ea9c772461b68f223
nkovacic99 <nace.kovacic007@gmail.com>
2020-03-20 13:12:41 +0100

    Reševal vaje od doma

---
+++ I/Razcep.java
@@ -0,0 +1,28 @@
+public class Razcep {
+
+  public static void main (String[] args) {
+    razcep(1024);
+    razcep(42);
+    razcep(17);
+    razcep(1);
+  }
+
+  public static void razcep (int n) {
+    char oper = '=';
+    System.out.print(n);
+    for (int d = 2; d * d <= n; ++d) {
+      int e = 0;
+      while (n % d == 0) {
+        n /= d;
+        ++e;
+      }
+      if (e > 0) {
+        System.out.print( " " + oper + " " + d);
+        if (e > 1) System.out.print("^" + e);
+        oper = '*';
+      }
+    }
+    if (n > 1) System.out.print(" " + oper + ' ' + n);
+    System.out.println();
+  }
+}
---
+++ II/Ena.java
@@ -0,0 +1,8 @@
+public class Ena {
+
+  public static void main (String[] args) {
+
+  }
+
+  
+}
